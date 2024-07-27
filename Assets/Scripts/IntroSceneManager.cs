cb5822ad984872406d45fe7c1776c329c7a7e3c8
Mike Fiari <fiarimike@yahoo.fr>
2024-07-27 15:54:21 +0200

    feat(intro) : add intro scene and create new map

---
+++ Assets/Scripts/IntroSceneManager.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class IntroSceneManager : MonoBehaviour
+{
+
+    [SerializeField]
+    private string newGameScene;
+
+    [SerializeField]
+    private Text introText;
+
+    [SerializeField]
+    private Scrollbar scrollbar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        AudioManager.instance.PlayBGM(4);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Submit"))
+        {
+            SceneManager.LoadScene(newGameScene);
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float speed = 0.013f;
+
+        scrollbar.value -= speed * Time.deltaTime;
+
+        if (scrollbar.value < 0)
+        {
+            SceneManager.LoadScene(newGameScene);
+        }
+    }
+}
